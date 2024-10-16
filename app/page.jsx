55d2acd68b22ff6ad30928183aa026498d68df5b
ruiyuuu <106572236+ruiyuuu@users.noreply.github.com>
2024-10-16 16:12:39 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -23,7 +23,7 @@ export default function Home() {
                     type: "bubble",
                     hero: {
                       type: "image",
-                      url: "https://test-line-liff-virid.vercel.app/test.png",
+                      url: `https://test-line-liff-virid.vercel.app/test.png?time=${new Date().getTime()}`,
                       size: "full",
                       aspectRatio: "13:13",
                       aspectMode: "cover",
@@ -38,7 +38,7 @@ export default function Home() {
                       contents: [
                         {
                           type: "text",
-                          text: "月圓你更圓02～",
+                          text: `月圓你更圓03～${new Date().getTime()}`,
                           weight: "bold",
                           size: "xl",
                         },
