617b9e311220b6c952542748f7a56bd86e3d4b32
ruiyuuu <106572236+ruiyuuu@users.noreply.github.com>
2024-10-16 15:41:15 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -23,7 +23,7 @@ export default function Home() {
                     type: "bubble",
                     hero: {
                       type: "image",
-                      url: "https://images.unsplash.com/photo-1453227588063-bb302b62f50b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                      url: "https://test-line-liff-virid.vercel.app/test.png",
                       size: "full",
                       aspectRatio: "13:13",
                       aspectMode: "cover",
